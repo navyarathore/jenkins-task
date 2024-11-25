ff1816c56df7d9c030b8b160f0143af363b34f9b
Navya Rathore <147660295+navyarathore@users.noreply.github.com>
2024-11-25 11:43:05 +0530

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo "Disk usage:"
-df | tail -1 | awk '{print $5}' | sed 's/%//'
+echo "Disk usage"
+
+disk_usage=$(df | tail -1 | awk '{print $5}' | sed 's/%//')
+
+if [ $disk_usage -gt 80 ]; then
+	echo "Disk utilization is above 80%"
+else
+	echo "Disk utilization is under 80%"
+fi
